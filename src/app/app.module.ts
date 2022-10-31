4d2672dae0823cb277b6ba34e9aa82ab831fc9f0
lucazed19 <lucazed19@outlook.com.br>
2022-10-31 14:53:42 -0300

    início site clientes

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PedidosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
